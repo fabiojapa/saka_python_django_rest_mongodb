40d542d7c8f6c1e14af7a242c6ac89c08a08dd4e
Fabio Sakamoto <fabiojapa@gmail.com>
2020-05-05 19:41:32 -0300

    make it better

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saka_python_django_rest_mongodb.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saka-python-djangorest-mongodb.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
