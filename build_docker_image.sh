361f586d5f7ffcf75be5a514c8cb15f16402bab0
Fabio Sakamoto <fabiojapa@gmail.com>
2020-05-07 03:21:15 -0300

    melhorias docker

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -f etc/docker/Dockerfile -t fabiojapa/saka_python_django_rest_mongodb .
+docker build --no-cache -f etc/docker/Dockerfile -t fabiojapa/saka_python_django_rest_mongodb .
 docker push fabiojapa/saka_python_django_rest_mongodb
