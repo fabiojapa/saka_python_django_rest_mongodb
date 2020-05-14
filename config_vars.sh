1c2d1804d75994d3cd91a96d2593382759f81916
fabio saka <fabiojapa@gmail.com>
2020-05-14 01:11:05 -0300

    config_vars.sh

--- config_vars.sh
+++ config_vars.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 #mongo free heroku
 #MONGODB_URI=mongodb://heroku_kmhlxl7x:2528hpu515spj8l4krs8icebtu@ds235840.mlab.com:35840/heroku_kmhlxl7x?retryWrites=true
 #MONGODB_USER=heroku_kmhlxl7x
 #MONGODB_PASSWORD=2528hpu515spj8l4krs8icebtu
 
 #mongo atlas gcp
-MONGODB_URI=mongodb+srv://appuser:sAbweK71bPwiaZww@googlesp-4dth2.gcp.mongodb.net/test?retryWrites=true
-MONGODB_USER=appuser
-MONGODB_PASSWORD=sAbweK71bPwiaZww
-export MONGODB_PASSWORD
-export MONGODB_USER
-export MONGODB_URI
+export MONGODB_URI="mongodb+srv://appuser:sAbweK71bPwiaZww@googlesp-4dth2.gcp.mongodb.net/test?retryWrites=true"
+export MONGODB_USER="appuser"
+export MONGODB_PASSWORD="sAbweK71bPwiaZww"
+
